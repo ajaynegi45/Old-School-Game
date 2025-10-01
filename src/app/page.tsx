a8c18d9459167252188e8290848d1f91daef6e83
Dhara Pandya <dharapandya2020@gmail.com>
2025-10-01 19:45:44 +0530

    remove npm run build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import Link from "next/link";
+//import Link from "next/link";
 import TicTacToeCardImage from "../../public/TicTacToaImageCard.png";
 import CollageImage from "../../public/CollageImage.png";
 import SudokuImage from "../../public/Sudoku.png";
