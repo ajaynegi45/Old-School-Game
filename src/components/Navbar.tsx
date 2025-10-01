a7590e0e97e2db9f2ce84e00204538cb2ebb3039
Dhara Pandya <dharapandya2020@gmail.com>
2025-10-01 19:22:53 +0530

    feat: Implement new 'Brain Train' UI redesign with pure CSS

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,16 +5,14 @@ import styles from "@/components/navbar.module.css";
 const Navbar = () => {
     return (
         <>
-            <div className={styles["navbar-container"]}>
-                <nav className={styles["navbar"]}>
-                    <Link href="/" className={styles["heading"]}>
-                        <h3 className={styles["title"]}>OLD SCHOOL GAME</h3>
+            <div className={styles.navbarContainer}>
+                <nav className={styles.navbar}>
+                    <Link href="/" className={styles.logo}>
+                        OLD SCHOOL GAME
                     </Link>
-                    <Link href="https://github.com/ajaynegi45/Old-School-Game" className={styles.link}>Github</Link>
+                    <Link href="https://github.com/ajaynegi45/Old-School-Game" target="_blank" className={styles.githubButton}>Github</Link>
                 </nav>
             </div>
-            <div className={styles["fade-navbar-effect"]}></div>
-            <div className={styles["empty-navbar"]}></div>
         </>
     );
 };
